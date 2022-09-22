41ec8ddc0890bd0095fd878b677724b6b5237195
Alexander Ryzhkov <ryzhkov9601@gmail.com>
2022-09-22 22:30:10 +0300

    added my decorator as custom parametrizer

--- test_Advert.py
+++ test_Advert.py
@@ -3,7 +3,7 @@ import json
 from Advert import Advert
 
 
-class Test_Advert:
+class TestAdvert:
     iphone = """{
         "title": "iPhone X",
         "price": 100,
@@ -27,40 +27,44 @@ class Test_Advert:
     data_without_price = '{"title": "my car"}'
     data_negative_price = '{"title": "house", "price": -10}'
 
+    def custom_parametrize(params, id_params=None):
+        return pytest.mark.parametrize(
+            'data_dict', params, ids=id_params, indirect=True)
+
     @pytest.fixture
     def data_dict(self, request):
         return json.loads(request.param)
 
-    @pytest.mark.parametrize(
-        'data_dict', [iphone, corgi],
-        ids=['iphone', 'corgi'], indirect=True)
+    @custom_parametrize([iphone, corgi], ['iphone', 'corgi'])
     def test_dot_notation(self, data_dict):
         assert Advert(data_dict).title == data_dict['title']
 
-    @pytest.mark.parametrize(
-        'data_dict', [iphone, corgi],
-        ids=['iphone', 'corgi'], indirect=True)
+    @custom_parametrize([iphone, corgi], ['iphone', 'corgi'])
     def test_nested_dot_notation(self, data_dict):
         addr = data_dict['location']['address']
         assert Advert(data_dict).location.address == addr
 
-    @pytest.mark.parametrize('data_dict', [data_without_title], indirect=True)
+    @custom_parametrize([data_without_title])
     def test_missing_title(self, data_dict):
         with pytest.raises(ValueError):
             Advert(data_dict)
 
-    @pytest.mark.parametrize('data_dict', [data_negative_price], indirect=True)
+    @custom_parametrize([data_negative_price])
     def test_negative_price(self, data_dict):
         with pytest.raises(ValueError):
             Advert(data_dict)
 
-    @pytest.mark.parametrize('data_dict', [data_without_price], indirect=True)
+    @custom_parametrize([data_without_price])
     def test_default_price(self, data_dict):
         assert Advert(data_dict).price == 0
 
+    testdata = [
+        (iphone, 'iPhone X | 100 ₽'),
+        (corgi, 'Вельш-корги | 1000 ₽'),
+    ]
+
     @pytest.mark.parametrize(
-        'data_dict, repr',
-        [(iphone, 'iPhone X | 100 ₽'), (corgi, 'Вельш-корги | 1000 ₽')],
+        'data_dict, repr', testdata,
         ids=['iphone', 'corgi'], indirect=['data_dict'])
     def test_repr(self, data_dict, repr):
         assert Advert(data_dict).__repr__() == repr
